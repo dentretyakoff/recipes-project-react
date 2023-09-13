1ca900db315414ee1d435870157b53cbeeced115
Денис Третьяков <den.tretyakoff@yandex.ru>
2023-09-13 15:55:23 +0700

    Пустая строка перед методами.

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class ReadOnly(BasePermission):
     """Права на чтение всем, без токена."""
+
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
